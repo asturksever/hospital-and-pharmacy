9082c7e75b79b2a17926ad40d224332b23c65293
Said Turksever <asturksever@gmail.com>
2020-04-04 19:03:39 +0300

    Hospital and pharmacies around

---
+++ configuration.js
@@ -0,0 +1,16 @@
+document.title = `${document.title} [Hospital and Pharmacy]`;
+
+let features = {
+  'amenity="pharmacy"':{
+    name:`Pharmacy`,
+    color:"#a700c8"
+  },
+  'amenity="hospital"':{
+    name:`Hospital`,
+    color:"#00d180"
+  },
+  'amenity="clinic"':{
+    name:`Clinic`,
+    color:"#000000"
+  }
+}
